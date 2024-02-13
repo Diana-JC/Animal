e38917ba3eb8ac136fdabb30b87fda99ecf50c93
dc552 <dc552@DESKTOP-80ATPO4>
2024-02-12 20:26:17 -0400

    Agregar archivos de proyecto.

---
+++ Animal/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+// Clase base
+class Animal
+{
+    public virtual void HacerSonido()
+    {
+        Console.WriteLine("El animal hace un sonido genérico.");
+    }
+}
+
+// Clases derivadas
+class Perro : Animal
+{
+    public override void HacerSonido()
+    {
+        Console.WriteLine("El perro ladra: ¡Guau, guau!");
+
+    }
+}
+
+class Gato : Animal
+{
+    public override void HacerSonido()
+    {
+        Console.WriteLine("El gato maulla: ¡Miau, miau!");
+
+ 
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Crear instancias de las clases derivadas
+        Perro miPerro = new Perro();
+        Gato miGato = new Gato();
+
+     
+
+        Console.WriteLine("\nMi perro:");
+        miPerro.HacerSonido();
+
+        Console.WriteLine("\nMi gato:");
+        miGato.HacerSonido();
+    }
+}
